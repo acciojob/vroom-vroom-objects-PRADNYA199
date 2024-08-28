8e9528bc95ee11f6deac1eb9f19bf15790973931
Pradnya Chandrakant Bhosale <67332759+PRADNYA199@users.noreply.github.com>
2024-08-28 09:16:45 -0700

    created a submission

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ function Car(make, model) {
 	this.make=make;
 	this.model=model;
 }
-Car.prototype.getMakeModel(){
+Car.prototype.getMakeModel()=function(){
 	return `${this.make} ${this.model}`;
 }
 
@@ -13,7 +13,7 @@ function SportsCar(make, model, topSpeed) {
 }
 SportsCar.prototype=Object.create(Car.prototype);
 SportsCar.prototype.constructor=SportsCar;
-SportsCar.prototype.getTopSpeed{
+SportsCar.prototype.getTopSpeed=function(){
 	return topSpeed;
 }
 
